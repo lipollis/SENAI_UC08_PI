42527317127492550c8cc9b90e0af7ea42f236e4
lipollis <aline.pollis@hotmail.com>
2022-03-14 03:28:36 -0300

    Publicando atividade PI Senai

---
+++ javascript/modal.js
@@ -0,0 +1,8 @@
+function abrirModal(){
+    document.getElementById('modal-ap').style.top="0";
+    document.getElementById('modal-ap').style.display="block";
+}
+function fecharModal(){
+    document.getElementById('modal-ap').style.top="-100%";
+    document.getElementById('modal-ap').style.display="none";
+}
